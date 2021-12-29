69ca544fdaa045c41ffa1f0fde42e9712c352bda
naamaah <naamaah@post.bgu.ac.il>
2021-12-29 13:20:16 +0200

    nothing

--- greatoded/excel2.py
+++
@@ -1,3 +0,0 @@
-from numpy import savetxt
-mylist=["1","2"]
-savetxt('data_shik.csv',mylist,fmt='%s')
--- greatoded/nuitrack/Examples/nuitrack_gl_sample/android/arm64-v8a/jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_OPTIM := release
-APP_ABI := arm64-v8a
-APP_PLATFORM := android-22
-APP_CPPFLAGS += -std=c++11 -fexceptions
-APP_STL := c++_shared
-NDK_TOOLCHAIN_VERSION := clang
